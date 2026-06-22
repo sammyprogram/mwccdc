475ad3ccb221cd70eb53da600a458b4796b9aac1
Samuel Grills <samwgrillz@gmail.com>
2026-06-21 23:13:54 -0500

    Hopefully final push

--- src/main/java/com/itccloud/mwccdc/controller/HomeController.java
+++ src/main/java/com/itccloud/mwccdc/controller/HomeController.java
@@ -1,11 +1,24 @@
 package com.itccloud.mwccdc.controller;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
-	public String index() {
-			return "index-form";
-	}
+
+    @GetMapping("/")
+    public String home(Model model) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String serverTime = LocalDateTime.now().format(formatter);
+
+        model.addAttribute("name", "Samuel G.");
+        model.addAttribute("major", "Cybersecurity");
+        model.addAttribute("serverTime", serverTime);
+
+        return "index-form.html";
+    }
 }
